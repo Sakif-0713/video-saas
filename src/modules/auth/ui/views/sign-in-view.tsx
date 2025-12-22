5f12086353de14ca5cf957b259093401876191c6
Sakif-0713 <moktadirskf1@gmail.com>
2025-12-23 04:29:18 +0600

    05 social auth

---
+++ src/modules/auth/ui/views/sign-in-view.tsx
@@ -0,0 +1,206 @@
+'use client'
+import { Card, CardContent } from '@/components/ui/card'
+import { email, z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Input } from '@/components/ui/input'
+import { FaGoogle, FaGithub } from 'react-icons/fa'
+import { Button } from '@/components/ui/button'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '@/components/ui/form'
+import { useForm } from 'react-hook-form'
+
+import { Alert, AlertTitle } from '@/components/ui/alert'
+import { OctagonAlertIcon } from 'lucide-react'
+import Link from 'next/link'
+import { useState } from 'react'
+import { authClient } from '@/lib/auth-client'
+import { on } from 'events'
+import { useRouter } from 'next/navigation'
+const formSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(1, {
+    message: 'Password is required'
+  })
+})
+
+export default function SignInView() {
+  const router = useRouter()
+  const [pending, setPending] = useState(false)
+  const [error, setError] = useState<string | null>(null)
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+      password: ''
+    }
+  })
+
+  const onSubmit = (data: z.infer<typeof formSchema>) => {
+    setError(null)
+    setPending(true)
+    authClient.signIn.email(
+      {
+        email: data.email,
+        password: data.password,
+        callbackURL: '/'
+      },
+      {
+        onSuccess: () => {
+          setPending(false)
+          router.push('/')
+        },
+        onError: ({ error }) => {
+          setError(error.message)
+        }
+      }
+    )
+  }
+
+  const onSocial = (provider: 'github' | 'google') => {
+    setError(null)
+    setPending(true)
+    authClient.signIn.social(
+      {
+        provider: provider,
+        callbackURL: '/'
+      },
+      {
+        onSuccess: () => {
+          setPending(false)
+        },
+        onError: ({ error }) => {
+          setError(error.message)
+        }
+      }
+    )
+  }
+
+  return (
+    <div className="flex flex-col gap-6">
+      <Card className="overflow-hidden p-0">
+        <CardContent className="grid p-0 md:grid-cols-2">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="p-6 md:p-8">
+              <div className="flex flex-col gap-6">
+                <div className="flex flex-col items-center text-center">
+                  <h1 className="text-2xl font-bold">Welcome back</h1>
+                  <p className="text-muted-foreground text-balance">
+                    Login to your account
+                  </p>
+                </div>
+                <div className="grid gap-3">
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                          <Input
+                            type="email"
+                            placeholder="m@example.com"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Password</FormLabel>
+                        <FormControl>
+                          <Input
+                            type="password"
+                            placeholder="Password"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                {!!error && (
+                  <Alert className="bg-destructive/10 border-none">
+                    <OctagonAlertIcon className="h-4 w-4 text-destructive!" />
+                    <AlertTitle>{error}</AlertTitle>
+                  </Alert>
+                )}
+                <Button disabled={pending} type="submit">
+                  Sign in
+                </Button>
+                <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+                  <span className="bg-card text-muted-foreground relative z-10 px-2">
+                    Or continue with
+                  </span>
+                </div>
+                <div className="grid grid-cols-2 gap-4">
+                  <Button
+                    disabled={pending}
+                    onClick={() => onSocial('google')}
+                    variant="outline"
+                    type="button"
+                  >
+                    <FaGoogle />
+                  </Button>
+                  <Button
+                    disabled={pending}
+                    variant="outline"
+                    type="button"
+                    onClick={() => onSocial('github')}
+                  >
+                    <FaGithub />
+                  </Button>
+                </div>
+                <div className="text-center text-sm">
+                  Don&apos;t have an account?{' '}
+                  <Link
+                    href="/sign-up"
+                    className="underline underline-offset-4"
+                  >
+                    Sign up
+                  </Link>
+                </div>
+              </div>
+            </form>
+          </Form>
+          <div className="bg-radial from-green-700 to-green-900 relative hidden md:flex flex-col gap-y-4 items-center justify-center">
+            <img src="/logo.svg" alt="logo" className="h-[92px] w-[92px]" />
+            <p className="text-2xl font-semibold text-white">Video-SaaS</p>
+          </div>
+        </CardContent>
+      </Card>
+
+      <div className="text-muted-foreground text-center text-xs text-balance ">
+        <p>
+          By clicking continue, you agree to our{' '}
+          <Link
+            href="/terms"
+            className="underline underline-offset-4 hover:text-primary"
+          >
+            Terms of Service
+          </Link>{' '}
+          and{' '}
+          <Link
+            href="/privacy"
+            className="underline underline-offset-4 hover:text-primary"
+          >
+            Privacy Policy
+          </Link>
+          .
+        </p>
+      </div>
+    </div>
+  )
+}
